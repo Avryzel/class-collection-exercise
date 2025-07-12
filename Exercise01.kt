8c2f836729b6918e14c2b6f2afb9e2423a581fea
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-07-12 19:09:10 +0700

    Completed Exercise 1 - group taks by priority

--- Exercise01.kt
+++ Exercise01.kt
@@ -10,3 +10,51 @@
 
     Bonus: Print the titles of the tasks for each group.
  */
+
+data class Task (
+    val title: String,
+    val priority: Priority,
+    val durationInMinutes: Int
+)
+
+enum class Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
+
+fun main() {
+    val listTask = mutableListOf(
+        Task(
+            title = "Morning stretch",
+            priority = Priority.LOW,
+            durationInMinutes = 10
+        ),
+        Task(
+            title = "Morning run",
+            priority = Priority.LOW,
+            durationInMinutes = 30
+        ),
+        Task(
+            title = "Learn Jetpack Compose",
+            priority = Priority.HIGH,
+            durationInMinutes = 60
+        ),
+        Task(
+            title = "Learn Japanese language",
+            priority = Priority.MEDIUM,
+            durationInMinutes = 45
+        ),
+        Task(
+            title = "Write a journal",
+            priority = Priority.MEDIUM,
+            durationInMinutes = 15
+        )
+    )
+
+    val groupedTask = listTask.groupBy { it.priority }
+
+    groupedTask.forEach { (priority, listTask) ->
+        println("$priority: ${listTask.size} tasks.")
+    }
+}
