38f4b03668119074e7843e6251116b2aa2a60973
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-07-12 19:27:54 +0700

    add Exercise 2 – Data Class & Extension Property task

---
+++ Exercise02.kt
@@ -0,0 +1,10 @@
+/*
+    Exercise 2
+
+    Task:
+
+    - Create a data class called Note with a title and content.
+    - Add an extension property to Note called preview that returns the first 10 characters of the content plus "..." if the content is longer than 10 characters.
+    - Create a list of at least 3 Note objects with different content lengths.
+    - Loop through the list and print each note’s title and its preview.
+*/
