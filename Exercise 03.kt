d38b394029372cbb0a6844da528e6f67ede9a805
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-07-14 23:03:03 +0700

    Complete Exercise 3 - Filter adult users

--- Exercise 03.kt
+++ Exercise 03.kt
@@ -8,3 +8,40 @@
     - Use filter to create a new list that contains only users who are 18 years old or older.
     - Print the names of the adult users.
 */
+
+data class User(
+    val name: String,
+    val age: Int
+)
+
+fun main() {
+    val userList = mutableListOf(
+        User(
+            name = "Mas Rusdi",
+            age = 20
+        ),
+        User(
+            name = "Si Imut",
+            age = 21
+        ),
+        User(
+            name = "Lil bro",
+            age = 15
+        ),
+        User(
+            name = "Kiddo",
+            age = 17
+        ),
+        User(
+            name = "Mas Amba",
+            age = 26
+        )
+    )
+
+    val adultUser = userList.filter { it.age >= 18 }
+
+    println("Adult User:")
+    adultUser.forEach {
+        println(it.name)
+    }
+}
