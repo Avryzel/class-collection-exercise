1a6b4eca5e5eb3cca3b5faa603f9dee15545c4dd
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-07-16 15:08:07 +0700

    Add Exercise 5 - Average student scores

---
+++ Exercise05.kt
@@ -0,0 +1,10 @@
+/*
+    Exercise 5
+
+    Task:
+
+    - Create a data class called Student with name and scores (List<Int>).
+    - Create a list of at least 4 Student objects with different sets of scores.
+    - Use map to create a list of average scores for each student.
+    - Use forEach to print each student's name and their average score.
+*/
