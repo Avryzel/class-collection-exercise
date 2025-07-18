c45789de6b059a75c18a71e101b47c5c0c7d660d
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-07-18 14:48:07 +0700

    Add Exercise 6 - Sort products by price

---
+++ Exercise06.kt
@@ -0,0 +1,10 @@
+/*
+    Exercise 6
+
+    Task:
+
+    - Reuse your Product data class (from Exercise 4).
+    - Create a list of at least 5 Product objects.
+    - Sort the list by price (from lowest to highest).
+    - Print each product name and its price.
+*/
