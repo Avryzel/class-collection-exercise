541b8f51a7d684c840e050cf69b396f213831720
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-07-18 14:22:36 +0700

    Complete Exercise 5 - Average student scores

--- Exercise05.kt
+++ Exercise05.kt
@@ -8,3 +8,24 @@
     - Use map to create a list of average scores for each student.
     - Use forEach to print each student's name and their average score.
 */
+
+data class Student(
+    val name: String,
+    val scores: List<Int>
+)
+
+fun main() {
+    val studentList = listOf(
+        Student("Jabib", listOf(45, 30, 40)),
+        Student("Je", listOf(45, 75, 80)),
+        Student("Wir", listOf(65, 78, 45)),
+        Student("San", listOf(75, 80, 55))
+    )
+
+    val averageList = studentList.map { it.scores.average() }
+
+    println("Student list:")
+    studentList.forEachIndexed { index, student ->
+        println("${student.name} - Average: ${averageList[index]}")
+    }
+}
